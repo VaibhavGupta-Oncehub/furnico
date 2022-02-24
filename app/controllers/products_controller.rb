1e8a846534c7902100aff826e6b27fb122c50bcc
vaibhav gupta <vaibhav.gupta@oncehub.com>
2022-02-24 19:32:33 +0530

    Deleted the previous images of products as they were too big and other related changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,8 @@
 class ProductsController < ApplicationController
     def index
+        #shows all the details of all the products
+    end
+    def show
+        #shows details about a single product
     end
 end
