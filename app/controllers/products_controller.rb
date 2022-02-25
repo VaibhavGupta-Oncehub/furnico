f23f34dbd870d42c8ab9fb23c38cdb784f03e0d9
vaibhav gupta <vaibhav.gupta@oncehub.com>
2022-02-25 16:17:30 +0530

    modified the product controllers and routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,10 @@
 class ProductsController < ApplicationController
+
+     
     def index
         #shows all the details of all the products
+        @products= Product.all
+        
     end
-    def show
-        #shows details about a single product
-    end
+   
 end
