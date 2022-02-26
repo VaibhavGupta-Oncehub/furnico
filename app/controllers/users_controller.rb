4eb3e2b2ff43c1d65a4310ca39898bc4516ad8bf
vaibhav gupta <vaibhav.gupta@oncehub.com>
2022-02-27 01:27:12 +0530

    Added signin, loggedin and loggedout sync with the current and logged in user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,15 @@ class UsersController < ApplicationController
   def index
     @users= User.all.paginate(page: params[:page], per_page: 3)
   end
-  
+
   def new 
     @user= User.new
   end
-  def create 
-    
-    @user= User.new(user_params)
 
+  def create 
+    @user= User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] ="User has been successfully created."
       redirect_to user_path(@user)
     else
@@ -23,9 +23,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    
     @user = User.find(params[:id])
-
   end
 
   def update
@@ -37,13 +35,9 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-
- 
-
+  
   private 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email , :password )
   end
-
- 
 end
