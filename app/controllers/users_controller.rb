039ac902d6b98f163f6e2cb3dfb3a6e7aaf751aa
vaibhav gupta <vaibhav.gupta@oncehub.com>
2022-02-26 16:03:44 +0530

    Added routes from signUp page to user page , flash messages and validations in the signUp form, deleted the login resources and finally now the functionality of signup page is working.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   def new 
+    @user= User.new
   end
   def create 
-
+    
+    @user= User.new(params.require(:user).permit(:first_name, :last_name, :email , :password_digest))
+   
+    if @user.save
+      flash[:notice] ="User has been successfully created."
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'login/show_signup'
-  get 'login/show_login'
-  get 'products/show_arrivals'
-  get 'products/show_features'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#home'  
   resources :users
 
-  get 'login/show_signup'
+  get 'login/show_signup', to: 'users#new'
   get 'login/show_login'
 
   get 'products/show_arrivals'
