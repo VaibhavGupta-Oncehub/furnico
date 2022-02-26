af41497c39f906fd95dcda9a319097c355fb9b3f
vaibhav gupta <vaibhav.gupta@oncehub.com>
2022-02-26 11:44:12 +0530

    deleted conflicting migrations and added products again in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_26_051551) do
+ActiveRecord::Schema.define(version: 2022_02_25_105105) do
 
   create_table "products", force: :cascade do |t|
     t.string "ProductName"
