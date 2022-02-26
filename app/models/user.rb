af8a55c73186d8bc07c925ba23706ac2e0db96d3
vaibhav gupta <vaibhav.gupta@oncehub.com>
2022-02-26 12:05:57 +0530

    Added associations between users and products as one-to-many relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  
+  has_many :products
   validates :first_name, presence: true,length: {minimum: 3, maximum:25}
   validates :last_name, presence: true, length: {minimum: 3, maximum:25}
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
