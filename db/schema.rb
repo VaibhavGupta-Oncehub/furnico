7a0aa142c956f22dc917c7ce2370a6dcc990be21
vaibhav gupta <vaibhav.gupta@oncehub.com>
2022-02-26 12:29:10 +0530

    added the bcrypt gem and has_secure_password to the user model with a password_digest field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_26_062645) do
+ActiveRecord::Schema.define(version: 2022_02_26_065443) do
 
   create_table "products", force: :cascade do |t|
     t.string "ProductName"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_02_26_062645) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
