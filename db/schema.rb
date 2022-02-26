4c572d7722cefe04e44af592d28e9dbde276ee4b
vaibhav gupta <vaibhav.gupta@oncehub.com>
2022-02-26 10:58:04 +0530

    Added user table and modal to the application

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_25_105105) do
+ActiveRecord::Schema.define(version: 2022_02_26_051551) do
 
   create_table "products", force: :cascade do |t|
     t.string "ProductName"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2022_02_25_105105) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ImageName"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
